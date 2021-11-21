a86db9d9c4b28bf451b04820aff552c9ad43ac97
jenniferpayano <arlenepayano@gmail.com>
2021-11-20 21:35:38 -0500

    cors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const auth = require('./lib/auth')
 // define server and client ports
 // used for cors and local port declaration
 const serverDevPort = 4741
-const clientDevPort = 7165
+// const clientDevPort = 7165
 
 // establish database connection
 // use new version of URL parser
@@ -39,7 +39,8 @@ const app = express()
 // Middleware
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-//app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
+// app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
+app.use(cors())
 app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
