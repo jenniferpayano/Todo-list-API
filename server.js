7527aa9fabca257ccef1dc2fc1906bbe0953e941
jenniferpayano <arlenepayano@gmail.com>
2023-01-15 20:24:18 -0500

    Updating to render

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const auth = require('./lib/auth')
 
 // define server and client ports
 // used for cors and local port declaration
-const serverDevPort = 4741
+const serverDevPort = 1337
 const clientDevPort = 7165
 
 // establish database connection
