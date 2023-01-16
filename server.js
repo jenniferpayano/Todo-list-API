574515b9669aac439b0049ce77d95c6fa2249eed
jenniferpayano <arlenepayano@gmail.com>
2023-01-15 20:45:05 -0500

    server cors ALL

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const app = express()
 // Middleware
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
+app.use(cors({ origin: '*' }))
 console.log(process.env.CLIENT_ORIGIN)
 // define port for API to run on
 const port = process.env.PORT || serverDevPort
